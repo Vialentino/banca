82f7869caceca9cd5caaedd22144c084b8d75e64
Vialentino <valentino.partesana@gmail.com>
2016-04-21 11:29:41 +0200

    corretto: da cartaCredito a Carta

--- src/corso/manpowerformazione/id148911/CartaCredito.java
+++ src/corso/manpowerformazione/id148911/CartaCredito.java
@@ -2,29 +2,4 @@ package corso.manpowerformazione.id148911;
 
 public class CartaCredito {
 
-	private final String pin;
-	private final CC cc;
-	private final String intestatario;
-	private final String scadenza;
-	private final Sportello sportello;
-	private final double commissione;
-	private final double maxGI;
-	private final double maxGO;
-	private final double maxMI;
-	private final double maxMO;
-	
-	
-	public CartaCredito(String pin, CC cc, String intestatario, String scadenza, Sportello sportello, double commissione, double GI, double GO, double MI, double MO){
-		this.pin = pin;
-		this.cc = cc;
-		this.intestatario = intestatario;
-		this.scadenza = scadenza;
-		this.commissione = commissione;
-		this.sportello = sportello;
-		this.maxGI=GI;
-		this.maxGO=GO;
-		this.maxMI=MI;
-		this.maxMO=MO;
-	}
-
 }
