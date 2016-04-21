a80e618704f88b2a7b05676426ece5d0a70827c6
gt8 <gt8>
2016-04-21 12:20:43 +0200

    Cliente,Intestatario,Persona pronti. Sportello in corso

--- src/corso/manpowerformazione/id148911/Intestatario.java
+++ src/corso/manpowerformazione/id148911/Intestatario.java
@@ -1,13 +1,19 @@
 package corso.manpowerformazione.id148911;
 
-public class Intestatario {
+import java.util.ArrayList;
+import corso.manpowerformazione.id148911.Carta;
+import corso.manpowerformazione.id148911.Persona;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class Intestatario extends Persona{
+	
+	 ArrayList<Carta> carte;
+	 
+	 public Intestatario(String nome, String cognome, String cf, Carta ... carte){
+		 
+		 super(nome,cognome,cf);
+		 for (Carta i : carte){
+			    this.carte.add(i);
+		      }
+	 }
+	
 }
