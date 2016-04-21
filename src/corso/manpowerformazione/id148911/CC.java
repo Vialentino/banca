5fd1d1773784faefbbc1de0da25fbbe9c53a8336
lorenzo <email@email.it>
2016-04-21 12:35:38 +0200

    primo commit

--- src/corso/manpowerformazione/id148911/CC.java
+++ src/corso/manpowerformazione/id148911/CC.java
@@ -1,17 +1,28 @@
 package corso.manpowerformazione.id148911;
 
-class Movimento {
-	
-}
+import java.util.ArrayList;
+
+import corso.manpowerformazione.id148911.Cliente;
 
 public class CC {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String iban;
+	private float saldo;
+	private Cliente cliente;
+	private float fido;
+	private ArrayList<Carta> carte=new ArrayList<Carta>();
+	private ArrayList<Movimento> movimenti=new ArrayList<Movimento>();
+	
+	public void addMovimento(Movimento m){
+		movimenti.add(m);
+		aggiornaSaldo();
+	}
+	
+	public void aggiornaSaldo(){
+		saldo=saldo+movimenti.get(movimenti.size()-1).getImporto();
+	}
+	
+	public boolean checkDisponibilita(float importo){
+		
 	}
 
 }
