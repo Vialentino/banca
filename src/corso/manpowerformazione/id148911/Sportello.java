78a44d676a6becdea9481632de982baea05c5d51
gt8 <gt8>
2016-04-22 11:42:49 +0200

    inserito argomento dove in Sportello. Iniziato metodo 'SceltaOperazione'

--- src/corso/manpowerformazione/id148911/Sportello.java
+++ src/corso/manpowerformazione/id148911/Sportello.java
@@ -2,26 +2,29 @@ package corso.manpowerformazione.id148911;
 
 import corso.manpowerformazione.id148911.Banca;
 import corso.manpowerformazione.id148911.Carta;
+import java.time.*;
 
 public class Sportello {
 
 	Banca banca;
+	String dove;
 	double disponibilita;
 	Carta carta;
 	
-	public Sportello(Banca banca, double disponibilita, Carta carta){
+	public Sportello(Banca banca, String dove, double disponibilita, Carta carta){
 		
 		this.banca=banca;
+		this.dove=dove;
 		this.disponibilita=disponibilita;
 		this.carta=carta;
 	}
 	
-	public void inserisciCarta(Carta unaCarta){
+	public void inserisciCarta(Carta carta){
 		
-		if (carta!=null){
+		if (this.carta!=null){
 			System.out.println("Impossibile inserire carta. Altra carta in fase di lettura.");
 		}
-		else carta=unaCarta;
+		else this.carta=carta;
 		
 	}
 	
@@ -30,7 +33,10 @@ public class Sportello {
 		if (carta==null){
 			System.out.println("Attenzione! Nessuna carta inserita.");
 		}
-		else carta=null;
+		else {
+			System.out.println("Ritirare la carta");
+			carta=null;
+		}
 		
 	}
     
@@ -43,7 +49,6 @@ public class Sportello {
     	if(carta.checkTetti()==0){
     		if(importoRichiesto<=carta.getCC().getSaldo()){
     			if(importoRichiesto <= disponibilita){
-    				//carta.getCC().addMovimento(Movimento(data,ora,causale,importo,dove));
         			System.out.println("Operazione andata a buon fine. Lei ha prelevato"+importoRichiesto);
        		        return importoRichiesto;
        		     }
@@ -81,6 +86,40 @@ public class Sportello {
     public void bloccaCarta(){
     }
     public void scegliOperazione(){
+    	
+    	System.out.println("Scegli operazione:");
+    	System.out.println("Prelievo --> digiti 1");
+    	System.out.println("Versamento --> digiti 2");
+    	System.out.println("Visualizza saldo & movimenti --> digiti 3");
+    	
+    	Scanner input1 = new Scanner(System.in);
+		int Scelta = input1.nextInt();
+		
+		if (Scelta == 1){
+			System.out.println("Inserisci importo desiderato");
+			Scanner input2 = new Scanner(System.in);
+			double importoRichiesto = input2.nextDouble();
+			double denaroInUscita = preleva(importoRichiesto);
+			if(denaroInUscita !=0){
+				LocalDateTime oggi=LocalDateTime.now();
+				String data = 
+						(oggi.getDayOfMonth()+"/"+
+				         oggi.getMonth().getValue()+"/"+
+				         oggi.getYear());
+				LocalTime adesso=LocalTime.now();
+				String ora =(adesso.getHour()+":"+adesso.getMinute());
+				String causale = "prelievo";
+				carta.getCC().addMovimento(Movimento(data,ora,causale,-importoRichiesto,dove));
+			}
+		}
+		else if (Scelta == 2);
+		else if (Scelta == 3);
+		else{
+			System.out.println(Scelta+" non è una scelta disponibile");
+			scegliOperazione();
+		}
+		espelliCarta();
+		
     }
     
     }
